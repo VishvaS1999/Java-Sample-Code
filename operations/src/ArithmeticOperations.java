8e3d4a9241fa5131b222bf721adfba3067f48b4e
VishvaS1999 <112763485+VishvaS1999@users.noreply.github.com>
2022-10-09 18:12:07 +0530

    Arithmetic Operations Using Scanner..

---
+++ operations/src/ArithmeticOperations.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations{
+
+    public static void main(String[] args) {
+
+        // Create scanner class object
+        Scanner in = new Scanner(System.in);
+
+        // Input two numbers from user
+        System.out.println("Enter first number :");
+        int num1 = in.nextInt();
+        System.out.println("Enter second number :");
+        int num2 = in.nextInt();
+
+
+        // Perform arithmetic operations.
+        int sum = num1 + num2;
+
+        int subtraction = num1 - num2;
+
+        int multiplication = num1 * num2;
+
+        int division = num1 / num2;
+
+        int modulo = num1 % num2;
+
+        // Print result to console.
+        System.out.println("Sum : " + sum);
+
+        System.out.println("subtraction : " + subtraction);
+
+        System.out.println("multiplication : " + multiplication);
+
+        System.out.println("division : " + division);
+
+        System.out.println("Modulo : " + modulo);
+    }
+}
+
+
